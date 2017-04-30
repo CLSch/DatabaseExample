e369dd60d26b49de098c1989bdbfa03b2b2fac1b
CLSch <caitlin.schaffers@gmail.com>
2017-04-30 12:37:18 +0200

    Versie om in de tutorial te gebruiken

--- app/src/main/java/com/example/caitlin/databaseexample/MainActivity.java
+++ app/src/main/java/com/example/caitlin/databaseexample/MainActivity.java
@@ -16,9 +16,6 @@ public class MainActivity extends AppCompatActivity {
     DBHelper helper;
     Context context;
     ArrayList<Contact> contactList;
-    TextView tv1;
-    TextView tv2;
-    TextView tv3;
     Contact contact;
 
     @Override
@@ -28,54 +25,17 @@ public class MainActivity extends AppCompatActivity {
 
         context = this;
         helper = new DBHelper(this);
-        tv1 = (TextView) findViewById(R.id.text);
-        tv2 = (TextView) findViewById(R.id.text2);
-        tv3 = (TextView) findViewById(R.id.text3);
 
         contact = new Contact("Mama", "0612345678");
 
         helper.create(contact);
 
-        showContact(1);
-    }
-
-    public void showContact(int tv) {
-        contactList = helper.read();
-        String contactInfo;
-
-        if (!contactList.isEmpty()) {
-            contactInfo = contactList.get(0).getName() + " - " + contactList.get(0).getNumber();
-        }
-        else {
-            contactInfo = "No contacts in database";
-        }
-
-        if (tv == 1) {
-            tv1.setText(contactInfo);
-            contact.setID(contactList.get(0).getID());
-        }
-        else if (tv == 2) {
-            tv1.setVisibility(View.INVISIBLE);
-            tv2.setText(contactInfo);
-            tv2.setVisibility(View.VISIBLE);
-        }
-        else {
-            tv2.setVisibility(View.INVISIBLE);
-            tv3.setText(contactInfo);
-            tv3.setVisibility(View.VISIBLE);
-        }
-
-    }
-
-    public void clicked1(View view) {
         contact.setNumber("0612121212");
-        int a = helper.update(contact);
-        Log.d("update gelukt?", Integer.toString(a));
-        showContact(2);
+        helper.update(contact);
+
+        helper.delete(contact);
+
+        contactList = helper.read();
     }
 
-    public void clicked2(View view) {
-        helper.delete(contact);
-        showContact(3);
-    }
 }
